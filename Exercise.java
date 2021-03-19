0ca10575948c905caaf7ad02e9e6a02d39bc10f7
rlmcmendes <kikomendes4@gmail.com>
2021-03-19 17:56:44 +0000

    all done

--- Exercise.java
+++ Exercise.java
@@ -1,20 +1,16 @@
 
-public class Exercise {
-	private int nPoints,nThreads;
+public class Exercise extends Thread {
 	private int counter=0;
-	private int numberOfThread=1;
-	private Thread[] tvec;
-	public Exercise(int numberOfPoints,int numberOfThreads)
+	private int numberOfPointsInThread;
+	public Exercise(int numberOfPointsInThreads)
 	{
-		this.nPoints=numberOfPoints;
-		this.nThreads=numberOfThreads;
-		tvec=new Thread[nThreads];
+		this.numberOfPointsInThread=numberOfPointsInThreads;
 	}
-	public class runnable implements Runnable {
+	
 
 		@Override
 		public void run() {
-			for(int j=0;j< (float ) (nPoints/ nThreads)*numberOfThread;j++) {
+			for(int j=0;j< numberOfPointsInThread;j++) {
 				double x=Math.random();
 				double y=Math.random();
 				if((Math.pow(x, 2)+Math.pow(y, 2))<=1) {
@@ -24,34 +20,10 @@ public class Exercise {
 			}
 			
 		}
-		
-	}
-	public void makeThreads() throws InterruptedException {
-		
-		for(int i=0;i<nThreads;i++,numberOfThread++) {
-		Thread t=new Thread(new Exercise(nPoints,nThreads).new runnable());
-		t.start();
-		tvec[(int) numberOfThread-1]=t;
+		public int getPointsInside()
+		{
+			return counter;
 		}
-		for(int i=0;i<nThreads;i++) {
-			tvec[i].join();
-		}
-	}
-	private boolean anyThreadAlive() {
-		for(int i=0;i<nThreads;i++) {
-			if(tvec[i].isAlive()) return true;
-		}
-		return false;
-	}
-	public double results() {
-		double res=0;
-		while(anyThreadAlive()) {
-			if(!anyThreadAlive()) {
-				res=4.0*( (double) (counter/nPoints));
-				break;
-			}
-		}
-		return res;
-	}
+
 	
 }
--- Main.java
+++ Main.java
@@ -11,12 +11,12 @@ import java.util.Scanner;
 public class Main {
 	public static volatile double counter=0;
 	public static volatile float numberOfThread=1;
+	
 	/**
 	 * 
 	 */
 	public Main() {
 		
-		// TODO Auto-generated constructor stub
 	}
 
 
@@ -25,12 +25,28 @@ public class Main {
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws InterruptedException {
+		long timeInitial=System.currentTimeMillis();
+		@SuppressWarnings("resource")
 		Scanner in=new Scanner(System.in);
 		int nPoints=in.nextInt();
 		int nThreads=in.nextInt();
-		Exercise e=new Exercise(nPoints,nThreads);
-		e.makeThreads();
-		System.out.println(e.results());
+		Exercise[] t=new Exercise[nThreads];
+		int nInside=0;
+		for(int i=0;i<nThreads;i++) {
+			Exercise e=new Exercise((int ) (nPoints/nThreads));
+			t[i]=e;
+			t[i].start();
+		}
+		for(int i=0;i<nThreads;i++) {
+			t[i].join();
+			nInside+=t[i].getPointsInside();
+		}
+		double ro= ((double ) nInside/ (double )nPoints);
+		double pi=4.0*ro;
+		long timefinal=System.currentTimeMillis();
+		System.out.println("numero do pi: "+pi);
+		System.out.println("total time: "+((timefinal-timeInitial)*0.001)+" segundos");
+		
 	}
 
 }
